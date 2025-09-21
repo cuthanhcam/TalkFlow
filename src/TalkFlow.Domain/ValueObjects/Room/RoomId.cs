bd78937dc43a5627f794aeaf8350afaa48efd29e
cuthanhcam <cuthanhcam04@gmail.com>
2025-09-21 22:50:54 +0700

    feat(domain): implement room value objects - RoomId, RoomCapacity, RoomName, SecurityCode

---
+++ src/TalkFlow.Domain/ValueObjects/Room/RoomId.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalkFlow.Domain.ValueObjects.Common;
+
+namespace TalkFlow.Domain.ValueObjects.Room
+{
+    public record RoomId : EntityId<RoomId>
+    {
+        public RoomId() : base(Guid.NewGuid()) { }
+
+        public RoomId(Guid value) : base(value) { }
+
+        public static RoomId New() => new(Guid.NewGuid());
+    }
+}
