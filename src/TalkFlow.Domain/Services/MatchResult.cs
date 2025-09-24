8b20c43889a13f67746f5a3e2596f3f814a4f238
cuthanhcam <cuthanhcam04@gmail.com>
2025-09-25 00:11:59 +0700

    feat(domain): add Domain/Services - IRoomDomainService - IStrangerMatchingService - IUserDomainService - IWebRTCSignalingService - MatchResult

---
+++ src/TalkFlow.Domain/Services/MatchResult.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalkFlow.Domain.ValueObjects.Room;
+using TalkFlow.Domain.ValueObjects.User;
+
+namespace TalkFlow.Domain.Services
+{
+    public class MatchResult
+    {
+        public bool IsSuccess { get; }
+        public RoomId? RoomId { get; }
+        public UserId? PartnerId { get; }
+        public string? ErrorMessage { get; }
+
+        public MatchResult(bool isSuccess, RoomId? roomId, UserId? partnerId, string? errorMessage)
+        {
+            IsSuccess = isSuccess;
+            RoomId = roomId;
+            PartnerId = partnerId;
+            ErrorMessage = errorMessage;
+        }
+
+        public static MatchResult Success(RoomId roomId, UserId partnerId)
+        {
+            return new MatchResult(true, roomId, partnerId, null);
+        }
+
+        public static MatchResult Failure(string errorMessage)
+        {
+            return new MatchResult(false, null, null, errorMessage);
+        }
+    }
+}
