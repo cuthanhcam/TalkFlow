d30b47923c3e72a2776c3f66623a1d58b456053b
cuthanhcam <cuthanhcam04@gmail.com>
2025-09-25 00:11:59 +0700

    feat(domain): add Domain/Services - IRoomDomainService - IStrangerMatchingService - IUserDomainService - IWebRTCSignalingService - MatchResult

---
+++ src/TalkFlow.Domain/Services/IStrangerMatchingService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalkFlow.Domain.Aggregates.User;
+using TalkFlow.Domain.ValueObjects.User;
+
+namespace TalkFlow.Domain.Services
+{
+    public interface IStrangerMatchingService
+    {
+        Task<IEnumerable<User>> FindMatchingUsersAsync(StrangerFilter filter, UserId excludeUserId);
+        Task<bool> CanMatchUsersAsync(UserId user1Id, UserId user2Id);
+        Task<MatchResult> TryMatchAsync(UserId requesterId);
+    }
+}
