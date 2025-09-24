b80eaa74a5a1150c7fb8b932cf07ca38fa486279
cuthanhcam <cuthanhcam04@gmail.com>
2025-09-25 00:27:37 +0700

    feat(domain): add Specifications/Room

---
+++ src/TalkFlow.Domain/Specifications/Room/RoomByCapacitySpecification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalkFlow.Domain.Specifications.Room
+{
+    public class RoomByCapacitySpecification : BaseSpecification<TalkFlow.Domain.Aggregates.Room.Room>
+    {
+        public RoomByCapacitySpecification(int minCapacity, int maxCapacity)
+            : base(r => r.Capacity.Value >= minCapacity && r.Capacity.Value <= maxCapacity)
+        {
+        }
+    }
+}
