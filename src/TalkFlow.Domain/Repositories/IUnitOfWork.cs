2bbc33985e9f7e9982c0f5e1e52ac2f7efa108b3
cuthanhcam <cuthanhcam04@gmail.com>
2025-09-25 00:17:50 +0700

    feat(domain): add Interfaces in Repositories

---
+++ src/TalkFlow.Domain/Repositories/IUnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalkFlow.Domain.Repositories
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IUserRepository Users { get; }
+        IRoomRepository Rooms { get; }
+        IMessageRepository Messages { get; }
+        IStrangerFilterRepository StrangerFilters { get; }
+        IMatchRepository Matches { get; }
+        IStrangerMatchingRepository StrangerMatchings { get; }
+
+        Task<int> SaveChangesAsync();
+        Task BeginTransactionAsync();
+        Task CommitTransactionAsync();
+        Task RollbackTransactionAsync();
+    }
+}
---
+++ src/TalkFlow.Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalkFlow.Domain.Aggregates.User;
+using TalkFlow.Domain.ValueObjects.Common;
+using TalkFlow.Domain.ValueObjects.User;
+
+namespace TalkFlow.Domain.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(UserId userId);
+        Task<User?> GetByDisplayNameAsync(DisplayName displayName);
+        Task<User?> GetByEmailAsync(Email email);
+        Task<IEnumerable<User>> GetUsersAsync(int skip = 0, int take = 10);
+        Task<IEnumerable<User>> GetUsersByFilterAsync(StrangerFilter filter, UserId excludeUserId);
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(UserId userId);
+        Task<bool> ExistsAsync(UserId userId);
+        Task<int> CountAsync();
+    }
+}
