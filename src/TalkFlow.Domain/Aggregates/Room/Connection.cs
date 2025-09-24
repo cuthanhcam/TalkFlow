dcfbc0906326ecc60a210ae21ef7c8b457409b3e
cuthanhcam <cuthanhcam04@gmail.com>
2025-09-24 23:37:35 +0700

    i i Please enter the commit message for your changes. Lines starting i with '#' will be ignored, and an empty message aborts the commit. i i On branch feature/phase-1-domain-aggregates i Changes to be committed: i	new file:   src/TalkFlow.Domain/Aggregates/Matching/Match/Match.cs i	new file:   src/TalkFlow.Domain/Aggregates/Matching/Match/MatchId.cs i	new file:   src/TalkFlow.Domain/Aggregates/Matching/StrangerMatching/StrangerMatching.cs i	new file:   src/TalkFlow.Domain/Aggregates/Matching/StrangerMatching/StrangerMatchingId.cs i	new file:   src/TalkFlow.Domain/Aggregates/Room/Connection.cs i	new file:   src/TalkFlow.Domain/Aggregates/Room/Message.cs i	new file:   src/TalkFlow.Domain/Aggregates/Room/Room.cs feature(domain): implement phase-1-domain-aggregate - Add Aggregates/User, Room, Matching aggregate root and value object

---
+++ src/TalkFlow.Domain/Aggregates/Room/Connection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalkFlow.Domain.ValueObjects.User;
+
+namespace TalkFlow.Domain.Aggregates.Room
+{
+    public class Connection
+    {
+        public string ConnectionId { get; private set; }
+        public UserId UserId { get; private set; }
+        public Guid RoomId { get; private set; }
+        public DateTime ConnectedAt { get; private set; }
+
+        protected Connection()
+        {
+            ConnectionId = string.Empty;
+            UserId = UserId.New();
+            RoomId = Guid.NewGuid();
+        }
+
+        public Connection(string connectionId, UserId userId, Guid roomId)
+        {
+            if (string.IsNullOrWhiteSpace(connectionId))
+                throw new ArgumentException("Connection ID cannot be null or empty", nameof(connectionId));
+
+            ConnectionId = connectionId;
+            UserId = userId;
+            RoomId = roomId;
+            ConnectedAt = DateTime.UtcNow;
+        }
+    }
+}
