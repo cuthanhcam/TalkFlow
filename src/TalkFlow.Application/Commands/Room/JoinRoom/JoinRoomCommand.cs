83993273b1515a9ff37910d27b0da6bffa2cf28e
cuthanhcam <cuthanhcam04@gmail.com>
2025-09-25 14:37:48 +0700

    feat(application): add Commands/Room/JoinRoom, UpdateRoom

---
+++ src/TalkFlow.Application/Commands/Room/JoinRoom/JoinRoomCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalkFlow.Application.Common.Interfaces;
+using TalkFlow.Application.DTOs.Common;
+using TalkFlow.Application.DTOs.Room;
+
+namespace TalkFlow.Application.Commands.Room.JoinRoom
+{
+    public record JoinRoomCommand(JoinRoomDto RoomData) : ICommand<Result<RoomDto>>;
+}
