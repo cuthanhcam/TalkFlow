f6593cdf47837face1ce91113095597191dd065e
cuthanhcam <cuthanhcam04@gmail.com>
2025-09-25 13:54:35 +0700

    feat(application): add Application/DTOs/Common, Message, Room, Users

---
+++ src/TalkFlow.Application/DTOs/Room/RoomDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalkFlow.Application.DTOs.Room
+{
+    public class RoomDto
+    {
+        public Guid RoomId { get; set; }
+        public string RoomName { get; set; } = string.Empty;
+        public Guid HostId { get; set; }
+        public string HostDisplayName { get; set; } = string.Empty;
+        public int MemberCount { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsChatBlocked { get; set; }
+        public bool IsActive { get; set; }
+        public string? SecurityCode { get; set; }
+    }
+}
