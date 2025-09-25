a2ed25df6c1af9cafb034867cc9475598348846e
cuthanhcam <cuthanhcam04@gmail.com>
2025-09-25 13:54:35 +0700

    feat(application): add Application/DTOs/Common, Message, Room, Users

---
+++ src/TalkFlow.Application/DTOs/Room/CreateRoomResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalkFlow.Application.DTOs.User;
+
+namespace TalkFlow.Application.DTOs.Room
+{
+    public class CreateRoomResponseDto
+    {
+        public UserDto User { get; set; } = new();
+        public RoomDto Room { get; set; } = new();
+    }
+}
---
+++ src/TalkFlow.Application/DTOs/User/StrangerFilterDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalkFlow.Application.DTOs.User
+{
+    public class StrangerFilterDto
+    {
+        public ICollection<string> FindGender { get; set; } = new List<string>();
+        public int MinAge { get; set; }
+        public int MaxAge { get; set; }
+        public ICollection<string> FindRegion { get; set; } = new List<string>();
+    }
+}
