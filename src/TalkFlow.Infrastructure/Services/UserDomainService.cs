9ab8d3c1ee5f6b8a4fa392b3e4e5768a34ddfdf4
cuthanhcam <cuthanhcam04@gmail.com>
2025-09-26 10:52:04 +0700

    feat(infrastructure): add Services

---
+++ src/TalkFlow.Infrastructure/Services/UserDomainService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalkFlow.Domain.Aggregates.User;
+using TalkFlow.Domain.Repositories;
+using TalkFlow.Domain.Services;
+using TalkFlow.Domain.ValueObjects.Common;
+using TalkFlow.Domain.ValueObjects.User;
+
+namespace TalkFlow.Infrastructure.Services
+{
+    public class UserDomainService : IUserDomainService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserDomainService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<bool> IsDisplayNameUniqueAsync(DisplayName displayName, UserId? excludeUserId = null)
+        {
+            var user = await _unitOfWork.Users.GetByDisplayNameAsync(displayName);
+            if (user == null) return true;
+            return excludeUserId != null && new UserId(user.Id) == excludeUserId;
+        }
+
+        public async Task<bool> IsEmailUniqueAsync(Email email, UserId? excludeUserId = null)
+        {
+            var user = await _unitOfWork.Users.GetByEmailAsync(email);
+            if (user == null) return true;
+            return excludeUserId != null && new UserId(user.Id) == excludeUserId;
+        }
+
+        public Task<User?> FindUserByDisplayNameAsync(DisplayName displayName)
+            => _unitOfWork.Users.GetByDisplayNameAsync(displayName);
+
+        public Task<User?> FindUserByEmailAsync(Email email)
+            => _unitOfWork.Users.GetByEmailAsync(email);
+    }
+}
